c4a580c8e62dde1a4d46ce5baf877f23f58ebd03
Uros Tomic <urostomic94@gmail.com>
2023-04-02 01:03:48 +0200

    remove cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import 'dotenv/config';
 import express from "express";
-import cors from "cors";
 import router from "./routes/pdf.route.js"
 
 const port = process.env.PORT || 8080
 
 const app = express()
-
-app.use(cors())
 
 app.listen(port, () => {
     app.use(router)
