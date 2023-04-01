cced4a56601beafec579a09e9b3e91a1edc3858d
Uros Tomic <urostomic94@gmail.com>
2023-04-02 00:03:01 +0200

    remove express-fileupload dependency

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import 'dotenv/config';
 import express from "express";
 import router from "./routes/pdf.route.js"
-import fileupload from "express-fileupload";
 
 const app = express()
 const port = process.env.PORT || 8080
-
-app.use(fileupload({
-    useTempFiles: true,
-    tempFileDir: "/tmp",
-}))
 
 app.listen(port, () => {
     app.use(router)
