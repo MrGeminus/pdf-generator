0bb2e6c07e42af917fb2efa3dad73382c5ae30bc
Uros Tomic <urostomic94@gmail.com>
2023-04-01 22:17:07 +0200

    puppeteer problem

--- src/controllers/pdf.controller.js
+++ src/controllers/pdf.controller.js
@@ -1,16 +1,27 @@
 import puppeteer from 'puppeteer';
 
 const generatePDF = async (req, res) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1200, height: 800, deviceScaleFactor: 2 });
-  await page.goto(req.headers.referer);
-  const element = await page.waitForSelector('#target');
-  const currentTime = new Date().getTime()
-  const screenshot = await element.screenshot({
-    path: `tmp/uplatnica-${currentTime}.png`
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-steuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote"
+    ],
+    executablePath: process.env.NODE.ENV === "production"
+      ? process.env.PUPPETEER_EXECUTABLE_PATH
+      : puppeteer.executablePath()
   });
-  await page.setContent(`
+  try {
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1200, height: 800, deviceScaleFactor: 2 });
+    await page.goto(req.headers.referer);
+    const element = await page.waitForSelector('#target');
+    const currentTime = new Date().getTime()
+    const screenshot = await element.screenshot({
+      path: `tmp/uplatnica-${currentTime}.png`
+    });
+    await page.setContent(`
     <!DOCTYPE html>
     <html>
         <head>
@@ -33,12 +44,18 @@ const generatePDF = async (req, res) => {
         </body>
     </html>
     `)
-  const pdf = await page.pdf({
-    path: `tmp/uplatnica-${currentTime}.pdf`,
-    format: 'A4'
-  })
-  await browser.close();
-  return res.download(`tmp/uplatnica-${currentTime}.pdf`)
+    const pdf = await page.pdf({
+      path: `tmp/uplatnica-${currentTime}.pdf`,
+      format: 'A4'
+    })
+    res.download(`tmp/uplatnica-${currentTime}.pdf`)
+  }
+  catch (e) {
+    res.send('Something went wrong')
+  }
+  finally {
+    await browser.close();
+  }
 }
 
 export default generatePDF
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
+import 'dotenv/config';
 import express from "express";
 import router from "./routes/pdf.route.js"
 import fileupload from "express-fileupload";
 
 const app = express()
+const port = process.env.PORT || 8080
 
 app.use(fileupload({
     useTempFiles: true,
     tempFileDir: "/tmp",
 }))
 
-app.listen(8080, () => {
+app.listen(port, () => {
     app.use(router)
 })
